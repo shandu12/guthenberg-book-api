3ad087faffed6f3f520ac2f5dfc162acf77e285e
shandu12 <93714524+shandu12@users.noreply.github.com>
2026-06-11 08:22:36 +0200

    minor fixes

--- src/Books api/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-const path = require('path')
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
-};
-
-export default nextConfig;
